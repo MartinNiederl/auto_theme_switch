1a7f5eabb6e6c8733d7dae6c10e999dbd3cf2de4
Martin Niederl <martin.niederl99@gmail.com>
2022-05-13 21:44:28 +0200

    Hide cmd when updating registry

--- auto_theme_switch/utils.py
+++ auto_theme_switch/utils.py
@@ -27,7 +27,7 @@ def set_wallpaper(path: str):
 
 def set_registry_value(key: str, value: str, data: str):
     command = ['reg.exe', 'add', key, '/v', value, '/t', 'REG_DWORD', '/d', data, '/f']
-    subprocess.run(command, stdout=subprocess.DEVNULL)
+    subprocess.run(command, stdout=subprocess.DEVNULL, creationflags=subprocess.CREATE_NO_WINDOW)
 
 
 def get_current_directory():
